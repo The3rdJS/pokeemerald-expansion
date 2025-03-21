9a99e4dcfb74b34984872848dee7c995b1eb301e
The3rdJS <the3rdjjs@gmail.com>
2025-03-21 23:16:54 +0000

    Rare Candy Crash (Proper Fix)

--- include/item_use.h
+++ include/item_use.h
@@ -21,6 +21,7 @@ void ItemUseOutOfBattle_SacredAsh(u8);
 void ItemUseOutOfBattle_PPRecovery(u8);
 void ItemUseOutOfBattle_PPUp(u8);
 void ItemUseOutOfBattle_RareCandy(u8);
+void Task_OpenRegisteredRareCandy(u8 taskId);
 void ItemUseOutOfBattle_DynamaxCandy(u8);
 void ItemUseOutOfBattle_TMHM(u8);
 void ItemUseOutOfBattle_Repel(u8);
