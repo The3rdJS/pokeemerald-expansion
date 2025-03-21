7f558c3fcd0079433b1a754ccd9fb803b2356134
The3rdJS <the3rdjjs@gmail.com>
2025-03-21 22:32:45 +0000

    All Trees Get Permanently Cut

--- include/constants/flags.h
+++ include/constants/flags.h
@@ -1222,28 +1222,28 @@
 #define FLAG_ITEM_SAFARI_ZONE_NORTH_EAST_NUGGET                     0x491
 #define FLAG_ITEM_SAFARI_ZONE_SOUTH_EAST_BIG_PEARL                  0x492
 
-#define FLAG_UNUSED_0x493                                           0x493 // Unused Flag
-#define FLAG_UNUSED_0x494                                           0x494 // Unused Flag
-#define FLAG_UNUSED_0x495                                           0x495 // Unused Flag
-#define FLAG_UNUSED_0x496                                           0x496 // Unused Flag
-#define FLAG_UNUSED_0x497                                           0x497 // Unused Flag
-#define FLAG_UNUSED_0x498                                           0x498 // Unused Flag
-#define FLAG_UNUSED_0x499                                           0x499 // Unused Flag
-#define FLAG_UNUSED_0x49A                                           0x49A // Unused Flag
-#define FLAG_UNUSED_0x49B                                           0x49B // Unused Flag
-#define FLAG_UNUSED_0x49C                                           0x49C // Unused Flag
-#define FLAG_UNUSED_0x49D                                           0x49D // Unused Flag
-#define FLAG_UNUSED_0x49E                                           0x49E // Unused Flag
-#define FLAG_UNUSED_0x49F                                           0x49F // Unused Flag
-#define FLAG_UNUSED_0x4A0                                           0x4A0 // Unused Flag
-#define FLAG_UNUSED_0x4A1                                           0x4A1 // Unused Flag
-#define FLAG_UNUSED_0x4A2                                           0x4A2 // Unused Flag
-#define FLAG_UNUSED_0x4A3                                           0x4A3 // Unused Flag
-#define FLAG_UNUSED_0x4A4                                           0x4A4 // Unused Flag
-#define FLAG_UNUSED_0x4A5                                           0x4A5 // Unused Flag
-#define FLAG_UNUSED_0x4A6                                           0x4A6 // Unused Flag
-#define FLAG_UNUSED_0x4A7                                           0x4A7 // Unused Flag
-#define FLAG_UNUSED_0x4A8                                           0x4A8 // Unused Flag
+#define FLAG_TREE_PETALBURGWOODS_1                                  0x493
+#define FLAG_TREE_PETALBURGWOODS_2                                  0x494
+#define FLAG_TREE_ROUTE103_1                                        0x495
+#define FLAG_TREE_ROUTE103_2                                        0x496
+#define FLAG_TREE_ROUTE104_1                                        0x497
+#define FLAG_TREE_ROUTE111_1                                        0x498
+#define FLAG_TREE_ROUTE116_1                                        0x499
+#define FLAG_TREE_ROUTE116_2                                        0x49A
+#define FLAG_TREE_ROUTE116_3                                        0x49B
+#define FLAG_TREE_ROUTE116_4                                        0x49C
+#define FLAG_TREE_ROUTE116_5                                        0x49D
+#define FLAG_TREE_ROUTE117_1                                        0x49E
+#define FLAG_TREE_ROUTE118_1                                        0x49F
+#define FLAG_TREE_ROUTE120_1                                        0x4A0
+#define FLAG_TREE_ROUTE120_2                                        0x4A1
+#define FLAG_TREE_ROUTE120_3                                        0x4A2
+#define FLAG_TREE_ROUTE121_1                                        0x4A3
+#define FLAG_TREE_ROUTE121_2                                        0x4A4
+#define FLAG_TREE_ROUTE121_3                                        0x4A5
+#define FLAG_TREE_ROUTE123_1                                        0x4A6
+#define FLAG_TREE_ROUTE123_2                                        0x4A7
+#define FLAG_TREE_ROUTE123_3                                        0x4A8
 #define FLAG_UNUSED_0x4A9                                           0x4A9 // Unused Flag
 #define FLAG_UNUSED_0x4AA                                           0x4AA // Unused Flag
 #define FLAG_UNUSED_0x4AB                                           0x4AB // Unused Flag
