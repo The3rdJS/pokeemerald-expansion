0e9e8120eda31011ed97c5e10758cfc660a74454
The3rdJS <the3rdjjs@gmail.com>
2026-07-03 09:38:20 +0100

    Trees and Rocks Get Permanently Destroyed

--- include/constants/flags.h
+++ include/constants/flags.h
@@ -664,68 +664,72 @@
 #define FLAG_HIDDEN_ITEM_ROUTE_123_RARE_CANDY                (FLAG_HIDDEN_ITEMS_START + 0x6E)
 #define FLAG_HIDDEN_ITEM_ROUTE_105_BIG_PEARL                 (FLAG_HIDDEN_ITEMS_START + 0x6F)
 
-#define FLAG_UNUSED_0x264  0x264 // Unused Flag
-#define FLAG_UNUSED_0x265  0x265 // Unused Flag
-#define FLAG_UNUSED_0x266  0x266 // Unused Flag
-#define FLAG_UNUSED_0x267  0x267 // Unused Flag
-#define FLAG_UNUSED_0x268  0x268 // Unused Flag
-#define FLAG_UNUSED_0x269  0x269 // Unused Flag
-#define FLAG_UNUSED_0x26A  0x26A // Unused Flag
-#define FLAG_UNUSED_0x26B  0x26B // Unused Flag
-#define FLAG_UNUSED_0x26C  0x26C // Unused Flag
-#define FLAG_UNUSED_0x26D  0x26D // Unused Flag
-#define FLAG_UNUSED_0x26E  0x26E // Unused Flag
-#define FLAG_UNUSED_0x26F  0x26F // Unused Flag
-#define FLAG_UNUSED_0x270  0x270 // Unused Flag
-#define FLAG_UNUSED_0x271  0x271 // Unused Flag
-#define FLAG_UNUSED_0x272  0x272 // Unused Flag
-#define FLAG_UNUSED_0x273  0x273 // Unused Flag
-#define FLAG_UNUSED_0x274  0x274 // Unused Flag
-#define FLAG_UNUSED_0x275  0x275 // Unused Flag
-#define FLAG_UNUSED_0x276  0x276 // Unused Flag
-#define FLAG_UNUSED_0x277  0x277 // Unused Flag
-#define FLAG_UNUSED_0x278  0x278 // Unused Flag
-#define FLAG_UNUSED_0x279  0x279 // Unused Flag
-#define FLAG_UNUSED_0x27A  0x27A // Unused Flag
-#define FLAG_UNUSED_0x27B  0x27B // Unused Flag
-#define FLAG_UNUSED_0x27C  0x27C // Unused Flag
-#define FLAG_UNUSED_0x27D  0x27D // Unused Flag
-#define FLAG_UNUSED_0x27E  0x27E // Unused Flag
-#define FLAG_UNUSED_0x27F  0x27F // Unused Flag
-#define FLAG_UNUSED_0x280  0x280 // Unused Flag
-#define FLAG_UNUSED_0x281  0x281 // Unused Flag
-#define FLAG_UNUSED_0x282  0x282 // Unused Flag
-#define FLAG_UNUSED_0x283  0x283 // Unused Flag
-#define FLAG_UNUSED_0x284  0x284 // Unused Flag
-#define FLAG_UNUSED_0x285  0x285 // Unused Flag
-#define FLAG_UNUSED_0x286  0x286 // Unused Flag
-#define FLAG_UNUSED_0x287  0x287 // Unused Flag
-#define FLAG_UNUSED_0x288  0x288 // Unused Flag
-#define FLAG_UNUSED_0x289  0x289 // Unused Flag
-#define FLAG_UNUSED_0x28A  0x28A // Unused Flag
-#define FLAG_UNUSED_0x28B  0x28B // Unused Flag
-#define FLAG_UNUSED_0x28C  0x28C // Unused Flag
-#define FLAG_UNUSED_0x28D  0x28D // Unused Flag
-#define FLAG_UNUSED_0x28E  0x28E // Unused Flag
-#define FLAG_UNUSED_0x28F  0x28F // Unused Flag
-#define FLAG_UNUSED_0x290  0x290 // Unused Flag
-#define FLAG_UNUSED_0x291  0x291 // Unused Flag
-#define FLAG_UNUSED_0x292  0x292 // Unused Flag
-#define FLAG_UNUSED_0x293  0x293 // Unused Flag
-#define FLAG_UNUSED_0x294  0x294 // Unused Flag
-#define FLAG_UNUSED_0x295  0x295 // Unused Flag
-#define FLAG_UNUSED_0x296  0x296 // Unused Flag
-#define FLAG_UNUSED_0x297  0x297 // Unused Flag
-#define FLAG_UNUSED_0x298  0x298 // Unused Flag
-#define FLAG_UNUSED_0x299  0x299 // Unused Flag
-#define FLAG_UNUSED_0x29A  0x29A // Unused Flag
-#define FLAG_UNUSED_0x29B  0x29B // Unused Flag
-#define FLAG_UNUSED_0x29C  0x29C // Unused Flag
-#define FLAG_UNUSED_0x29D  0x29D // Unused Flag
-#define FLAG_UNUSED_0x29E  0x29E // Unused Flag
-#define FLAG_UNUSED_0x29F  0x29F // Unused Flag
-#define FLAG_UNUSED_0x2A0  0x2A0 // Unused Flag
-#define FLAG_UNUSED_0x2A1  0x2A1 // Unused Flag
+// Cuttable Trees
+#define FLAG_CUTTABLE_TREE_PETALBURG_WOODS_1 0x264
+#define FLAG_CUTTABLE_TREE_PETALBURG_WOODS_2 0x265
+#define FLAG_CUTTABLE_TREE_ROUTE_103_1       0x266
+#define FLAG_CUTTABLE_TREE_ROUTE_103_2       0x267
+#define FLAG_CUTTABLE_TREE_ROUTE_104         0x268
+#define FLAG_CUTTABLE_TREE_ROUTE_111         0x269
+#define FLAG_CUTTABLE_TREE_ROUTE_116_1       0x26A
+#define FLAG_CUTTABLE_TREE_ROUTE_116_2       0x26B
+#define FLAG_CUTTABLE_TREE_ROUTE_116_3       0x26C
+#define FLAG_CUTTABLE_TREE_ROUTE_116_4       0x26D
+#define FLAG_CUTTABLE_TREE_ROUTE_116_5       0x26E
+#define FLAG_CUTTABLE_TREE_ROUTE_117         0x26F
+#define FLAG_CUTTABLE_TREE_ROUTE_118         0x270
+#define FLAG_CUTTABLE_TREE_ROUTE_120_1       0x271
+#define FLAG_CUTTABLE_TREE_ROUTE_120_2       0x272
+#define FLAG_CUTTABLE_TREE_ROUTE_120_3       0x273
+#define FLAG_CUTTABLE_TREE_ROUTE_121_1       0x274
+#define FLAG_CUTTABLE_TREE_ROUTE_121_2       0x275
+#define FLAG_CUTTABLE_TREE_ROUTE_121_3       0x276
+#define FLAG_CUTTABLE_TREE_ROUTE_123_1       0x277
+#define FLAG_CUTTABLE_TREE_ROUTE_123_2       0x278
+#define FLAG_CUTTABLE_TREE_ROUTE_123_3       0x279
+
+// Breakable Rocks
+#define FLAG_BREAKABLE_ROCK_GRANITE_CAVE_B2F_1        0x27A
+#define FLAG_BREAKABLE_ROCK_GRANITE_CAVE_B2F_2        0x27B
+#define FLAG_BREAKABLE_ROCK_GRANITE_CAVE_B2F_3        0x27C
+#define FLAG_BREAKABLE_ROCK_GRANITE_CAVE_B2F_4        0x27D
+#define FLAG_BREAKABLE_ROCK_GRANITE_CAVE_B2F_5        0x27E
+#define FLAG_BREAKABLE_ROCK_GRANITE_CAVE_B2F_6        0x27F
+#define FLAG_BREAKABLE_ROCK_GRANITE_CAVE_B2F_7        0x280
+#define FLAG_BREAKABLE_ROCK_MIRAGE_TOWER_3F_1         0x281
+#define FLAG_BREAKABLE_ROCK_MIRAGE_TOWER_3F_2         0x282
+#define FLAG_BREAKABLE_ROCK_MIRAGE_TOWER_4F           0x283
+#define FLAG_BREAKABLE_ROCK_ROUTE_111_1               0x284
+#define FLAG_BREAKABLE_ROCK_ROUTE_111_2               0x285
+#define FLAG_BREAKABLE_ROCK_ROUTE_114_1               0x286
+#define FLAG_BREAKABLE_ROCK_ROUTE_114_2               0x287
+#define FLAG_BREAKABLE_ROCK_ROUTE_114_3               0x288
+#define FLAG_BREAKABLE_ROCK_ROUTE_114_4               0x289
+#define FLAG_BREAKABLE_ROCK_ROUTE_114_5               0x28A
+#define FLAG_BREAKABLE_ROCK_ROUTE_115                 0x28B
+#define FLAG_BREAKABLE_ROCK_SAFARI_ZONE_NORTH_1       0x28C
+#define FLAG_BREAKABLE_ROCK_SAFARI_ZONE_NORTH_2       0x28D
+#define FLAG_BREAKABLE_ROCK_SAFARI_ZONE_NORTH_3       0x28E
+#define FLAG_BREAKABLE_ROCK_SAFARI_ZONE_NORTH_4       0x28F
+#define FLAG_BREAKABLE_ROCK_SAFARI_ZONE_NORTH_5       0x290
+#define FLAG_BREAKABLE_ROCK_SAFARI_ZONE_NORTH_6       0x291
+#define FLAG_BREAKABLE_ROCK_SAFARI_ZONE_NORTH_EAST_1  0x292
+#define FLAG_BREAKABLE_ROCK_SAFARI_ZONE_NORTH_EAST_2  0x293
+#define FLAG_BREAKABLE_ROCK_SAFARI_ZONE_NORTH_EAST_3  0x294
+#define FLAG_BREAKABLE_ROCK_SAFARI_ZONE_NORTH_EAST_4  0x295
+#define FLAG_BREAKABLE_ROCK_SAFARI_ZONE_NORTH_EAST_5  0x296
+#define FLAG_BREAKABLE_ROCK_SEAFLOOR_CAVERN_ROOM_1    0x297
+#define FLAG_BREAKABLE_ROCK_SEAFLOOR_CAVERN_ROOM_2_1  0x298
+#define FLAG_BREAKABLE_ROCK_SEAFLOOR_CAVERN_ROOM_2_2  0x299
+#define FLAG_BREAKABLE_ROCK_SEAFLOOR_CAVERN_ROOM_5_1  0x29A
+#define FLAG_BREAKABLE_ROCK_SEAFLOOR_CAVERN_ROOM_5_2  0x29B
+#define FLAG_BREAKABLE_ROCK_VICTORY_ROAD_B1F_1        0x29C
+#define FLAG_BREAKABLE_ROCK_VICTORY_ROAD_B1F_2        0x29D
+#define FLAG_BREAKABLE_ROCK_VICTORY_ROAD_B1F_3        0x29E
+#define FLAG_BREAKABLE_ROCK_VICTORY_ROAD_B1F_4        0x29F
+#define FLAG_BREAKABLE_ROCK_VICTORY_ROAD_B1F_5        0x2A0
+#define FLAG_BREAKABLE_ROCK_VICTORY_ROAD_B1F_6        0x2A1
+
 #define FLAG_UNUSED_0x2A2  0x2A2 // Unused Flag
 #define FLAG_UNUSED_0x2A3  0x2A3 // Unused Flag
 #define FLAG_UNUSED_0x2A4  0x2A4 // Unused Flag
